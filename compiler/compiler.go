b7d2d4e5010386f29888dbddd2ddcd796b046253
stan wielga <svwielga4@gmail.com>
2019-04-27 13:42:18 -0700

    added strings support

--- compiler/compiler.go
+++ compiler/compiler.go
@@ -185,6 +185,10 @@ func (c *Compiler) Compile(node ast.Node) error {
 			return fmt.Errorf("undefined variable %s", node.Value)
 		}
 		c.emit(code.OpGetGlobal, symbol.Index)
+
+	case *ast.StringLiteral:
+		str := &object.String{Value: node.Value}
+		c.emit(code.OpConstant, c.addConstant(str))
 	}
 
 	return nil
